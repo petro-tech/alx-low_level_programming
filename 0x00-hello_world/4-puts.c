88ca3071e6ea5af10ec6faf7187b1ae273190899
petro-tech <peteronialaexists@gmail.com>
2023-08-12 11:52:41 -0700

    c Basics

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+/**
+   *this is the main function in c program
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
