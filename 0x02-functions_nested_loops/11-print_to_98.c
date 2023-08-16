9645a04c241ac5243990e07d5aaf3342b89dc85e
petro-tech <peteronialaexists@gmail.com>
2023-08-16 05:32:44 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+#include<stdio.h>
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
  * Return: Always 0.
  */
-
 void print_to_98(int n)
-
 {
-
 	if (n <= 98)
 	{
 	for (; n <= 98; n++)
@@ -43,4 +40,3 @@ void print_to_98(int n)
 	}
 	}
 }
-	
