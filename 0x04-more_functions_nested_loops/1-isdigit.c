a537d8074b2a4e7f8408590a0b89c55f93e7d4fb
petro-tech <peteronialaexists@gmail.com>
2023-08-17 07:17:52 -0700

    checking for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - checking for digits
+ * @c: The character that is checked by the function
+ *Return: 1 for digit character or 0 for anything else
+ */
+
+int _isdigit(int c)
+
+{
+if (c >= 45 && c <= 57)
+{
+return (1);
+}
+return (0);
+}
